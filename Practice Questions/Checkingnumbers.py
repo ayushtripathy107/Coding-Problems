fec4c39372583a013a0d7d06fa2f133f2bdad0c8
Ayush Tripathy <ayushtripathy07@gmail.com>
2026-05-16 16:44:35 +0530

    Fix comment formatting in Checkingnumbers.py
    Corrected comment formatting and removed extra characters.

---
+++ Practice Questions/Checkingnumbers.py
@@ -0,0 +1,9 @@
+#Check whether given integer is positive, negative or zero
+
+a = int(input("Enter a integer: "))
+if a > 0:
+   print(a,"is positive integer")
+elif a == 0:
+   print(a,"Zero")
+else:
+   print(a,"is negative integer")
