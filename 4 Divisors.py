fe6aacc5ed9c771cf6e01fd78b288efd5d79c6b4
Ayush Tripathy <ayushtripathy07@gmail.com>
2026-05-29 20:46:51 +0530

    Implement sumFourDivisors method in Solution class

---
+++ 4 Divisors.py
@@ -0,0 +1,26 @@
+import math
+
+class Solution(object):
+    def sumFourDivisors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total_sum = 0
+        
+        for num in nums:
+            divisors = set()
+            # Iterate up to the square root of the number
+            for i in range(1, int(math.sqrt(num)) + 1):
+                if num % i == 0:
+                    divisors.add(i)
+                    divisors.add(num // i)
+                    # If we already have more than 4, stop early
+                    if len(divisors) > 4:
+                        break
+            
+            # Check if the number has exactly four divisors
+            if len(divisors) == 4:
+                total_sum += sum(divisors)
+                
+        return total_sum
